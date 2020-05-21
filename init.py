f8255d15fd9d187b1731b51fdcc9589a8e68b46d
exertieb <bastien.exertier@grenoble-inp.org>
2020-05-21 11:53:18 +0900

    corrections

--- init.py
+++ init.py
@@ -8,22 +8,22 @@ def create_folder_if_doesnt_exit(path, foldername):
 	""" cree le dossier si il n'existe pas deja """
 	full_path = path + foldername
 	if not os.path.isdir(full_path):
-		print('* {} doesnt exist'.format(full_path))
+		print(' * {} doesnt exist'.format(full_path))
 		os.mkdir(full_path)
-		print('* {} created'.format(full_path))
+		print(' * {} created'.format(full_path))
 	else:
-		print('* {} already exists'.format(full_path))
+		print(' * {} already exists'.format(full_path))
 
 def main():
 	""" main """
 	create_folder_if_doesnt_exit(PATH, 'uploads')
 	create_folder_if_doesnt_exit(PATH, 'qrcodes')
 	if not os.path.isfile(DBNAME):
-		print('* database doesnt exist')
+		print(' * database doesnt exist')
 		db_init_main()
-		print('* database initialized')
+		print(' * database initialized')
 	else:
-		print('* database already exists')
+		print(' * database already exists')
 
 if __name__ == '__main__':
 	main()
--- path.py
+++ path.py
@@ -1,15 +1,4 @@
 """ path du jeu """
 
-import os
-
-if 'PICTUREGAMEPATH' not in os.environ:
-	print('Please set your PICTUREGAMEPATH variable')
-	exit(0)
-
-PATH = os.environ['PICTUREGAMEPATH']
-
-if 'PICTUREGAMEURL' not in os.environ:
-	print('Please set your PICTUREGAMEURL variable')
-	exit(0)
-
-URL = os.environ['PICTUREGAMEURL']
+PATH = '/home/DigitalBananaDirector/picturegame/'
+URL = 'http://digitalbananadirector.pythonanywhere.com/'
